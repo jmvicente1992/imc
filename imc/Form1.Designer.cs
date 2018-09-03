54c5bf8520ab1733a94452a7c2f5c2a14180decc
killerprime666 <jogojefim@gmail.com>
2018-09-03 12:36:38 -0300

    1 Commit do projeto IMC

---
+++ imc/Form1.Designer.cs
@@ -0,0 +1,322 @@
+﻿namespace imc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblImc1 = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.txtPeso = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblImc = new System.Windows.Forms.Label();
+            this.txtImc = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.btnSalvar = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblImc1
+            // 
+            this.lblImc1.AutoSize = true;
+            this.lblImc1.Font = new System.Drawing.Font("Comic Sans MS", 24F, ((System.Drawing.FontStyle)(((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic) 
+                | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImc1.Location = new System.Drawing.Point(618, 9);
+            this.lblImc1.Name = "lblImc1";
+            this.lblImc1.Size = new System.Drawing.Size(85, 45);
+            this.lblImc1.TabIndex = 0;
+            this.lblImc1.Text = "IMC";
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPeso.Location = new System.Drawing.Point(492, 61);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(78, 27);
+            this.lblPeso.TabIndex = 1;
+            this.lblPeso.Text = "PESO :";
+            // 
+            // txtPeso
+            // 
+            this.txtPeso.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPeso.ForeColor = System.Drawing.Color.Red;
+            this.txtPeso.Location = new System.Drawing.Point(497, 91);
+            this.txtPeso.Name = "txtPeso";
+            this.txtPeso.Size = new System.Drawing.Size(120, 30);
+            this.txtPeso.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(712, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "ALTURA :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAltura.ForeColor = System.Drawing.Color.Red;
+            this.txtAltura.Location = new System.Drawing.Point(717, 91);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(120, 30);
+            this.txtAltura.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label2.Location = new System.Drawing.Point(563, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(192, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "peso / (altura * altura)";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnCalcular.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(567, 146);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(188, 38);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblImc
+            // 
+            this.lblImc.AutoSize = true;
+            this.lblImc.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImc.Location = new System.Drawing.Point(629, 276);
+            this.lblImc.Name = "lblImc";
+            this.lblImc.Size = new System.Drawing.Size(74, 30);
+            this.lblImc.TabIndex = 7;
+            this.lblImc.Text = "IMC :";
+            // 
+            // txtImc
+            // 
+            this.txtImc.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtImc.ForeColor = System.Drawing.Color.Red;
+            this.txtImc.Location = new System.Drawing.Point(586, 309);
+            this.txtImc.Name = "txtImc";
+            this.txtImc.Size = new System.Drawing.Size(152, 30);
+            this.txtImc.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(581, 380);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(161, 30);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "RESULTADO :";
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.Font = new System.Drawing.Font("Comic Sans MS", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.ForeColor = System.Drawing.Color.Red;
+            this.txtResultado.Location = new System.Drawing.Point(586, 414);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(152, 30);
+            this.txtResultado.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(493, 536);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(311, 133);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Legenda < 18.5 - Abaixo do Peso\r\n             \r\n                  18.5 a 24.9 - P" +
+    "eso Ideal\r\n\r\n                   25 a 29.9 - Excesso de Peso\r\n\r\n                 " +
+    ">30 - Obesidade\r\n";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnLimpar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(567, 210);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(188, 38);
+            this.btnLimpar.TabIndex = 12;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(747, 9);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(26, 20);
+            this.txtId.TabIndex = 13;
+            // 
+            // btnSalvar
+            // 
+            this.btnSalvar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnSalvar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSalvar.Location = new System.Drawing.Point(567, 450);
+            this.btnSalvar.Name = "btnSalvar";
+            this.btnSalvar.Size = new System.Drawing.Size(188, 38);
+            this.btnSalvar.TabIndex = 14;
+            this.btnSalvar.Text = "Salvar";
+            this.btnSalvar.UseVisualStyleBackColor = false;
+            this.btnSalvar.Click += new System.EventHandler(this.btnSalvar_Click);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(37, 61);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(97, 30);
+            this.lblNome.TabIndex = 15;
+            this.lblNome.Text = "NOME :";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.ForeColor = System.Drawing.Color.Red;
+            this.txtNome.Location = new System.Drawing.Point(30, 91);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(427, 30);
+            this.txtNome.TabIndex = 16;
+            // 
+            // dgv
+            // 
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.GridColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dgv.Location = new System.Drawing.Point(30, 186);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(540, 302);
+            this.dgv.TabIndex = 17;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnBuscar.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBuscar.Location = new System.Drawing.Point(30, 525);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(188, 38);
+            this.btnBuscar.TabIndex = 18;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.btnExcluir.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExcluir.Location = new System.Drawing.Point(269, 525);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(188, 38);
+            this.btnExcluir.TabIndex = 19;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = false;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(842, 678);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.dgv);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.btnSalvar);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtImc);
+            this.Controls.Add(this.lblImc);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPeso);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblImc1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CALCULADORA DE IMC";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblImc1;
+        private System.Windows.Forms.Label lblPeso;
+        private System.Windows.Forms.TextBox txtPeso;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblImc;
+        private System.Windows.Forms.TextBox txtImc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button btnSalvar;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnExcluir;
+    }
+}
+
---
+++ imc/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace imc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double v1 = Convert.ToDouble(txtPeso.Text);
+            double v2 = Convert.ToDouble(txtAltura.Text);
+            double imc;
+
+            imc = v1 / (v2 * v2);
+            txtImc.Text = imc.ToString();
+
+            if (imc <18.5)
+            {
+                txtResultado.Text = "Abaixo do peso";
+            }
+            else
+            
+                if (imc >=18.5 && imc <= 24.9)
+                {
+                    txtResultado.Text = "Peso Ideal";
+                }
+
+               else
+               
+               if (imc >25 && imc <=29.9)
+                {
+                    txtResultado.Text = "Excesso de peso";
+                }
+
+                else
+                {
+                    txtResultado.Text = "Obesidade";
+                }
+            }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtAltura.Clear();
+            txtImc.Clear();
+            txtPeso.Clear();
+            txtResultado.Clear();
+            txtNome.Clear();
+            
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection("Persist Security info=False;Server=127.0.0.1;Port=3307;" + "Database=bd_imc;Uid=root;Pwd=usbw;");
+            conexao.Open();
+
+            MySqlCommand comando;
+            if (txtId.Text == String.Empty)
+            {
+                comando = new MySqlCommand("INSERT INTO tb_imc(ID, PESO, ALTURA, IMC, RESULTADO, NOME)" + "VALUES('" + txtId.Text + "','" + txtPeso.Text + "','" + txtAltura.Text + "','" + txtImc.Text + "','" + txtResultado.Text + "', '" + txtNome.Text + "');", conexao);
+            }
+            else
+            {
+                comando = new MySqlCommand("Update tb_imc SET PESO='" + txtPeso.Text + "',ALTURA='" + txtAltura.Text + "', IMC='" + txtImc.Text + "', RESULTADO='" + txtResultado.Text + "', NOME='"+txtNome.Text+"'WHERE ID='" + txtId.Text + "'", conexao);
+            }
+            comando.ExecuteNonQuery();
+            MessageBox.Show("SALVO COM SUCESSO", "ATENÇÃO");
+            conexao.Close();
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string nome = dgv.CurrentRow.Cells[0].Value.ToString();
+            int id = Convert.ToInt32(dgv.CurrentRow.Cells[1].Value.ToString());
+            string peso = dgv.CurrentRow.Cells[2].Value.ToString();
+            string altura = dgv.CurrentRow.Cells[3].Value.ToString();
+            string imc = dgv.CurrentRow.Cells[4].Value.ToString();
+            string resultado = dgv.CurrentRow.Cells[5].Value.ToString();
+
+           
+            txtNome.Text = nome;
+            txtId.Text = id.ToString();
+            txtPeso.Text = peso;
+            txtAltura.Text = altura;
+            txtImc.Text = imc;
+            txtResultado.Text = resultado;
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection("Persist Security info=False;Server=127.0.0.1;Port=3307;" + "Database=bd_imc;Uid=root;Pwd=usbw;");
+            conexao.Open();
+
+            MySqlCommand pesquisa = new MySqlCommand("SELECT * FROM tb_imc ", conexao);
+
+            MySqlDataAdapter da = new MySqlDataAdapter(pesquisa);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dgv.DataSource = dt;
+
+            conexao.Close();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            MySqlConnection conexao = new MySqlConnection("Persist Security info=False;Server=127.0.0.1;Port=3307;" + "Database=bd_imc;Uid=root;Pwd=usbw;");
+
+            conexao.Open();
+
+            MySqlCommand deleta = new MySqlCommand("DELETE FROM tb_imc WHERE ID = '" + txtId.Text + "';", conexao);
+
+            MySqlDataAdapter da = new MySqlDataAdapter(deleta);
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dgv.DataSource = dt;
+
+            conexao.Close();
+        }
+    }
+    }
+
